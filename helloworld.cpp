60de34adfd222872a9e117a2a03e44c414ee481d
Naveeharn Tehmarn <naveeharntehmarn@MacBook-Pro-khxng-Naveeharn.local>
2020-08-09 21:37:20 +0700

    add input

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,11 +6,17 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    // vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
-    for (const string& word : msg)
-    {
-        cout << word << " \n";
-    }
-    cout << endl;
+    // for (const string& word : msg)
+    // {
+    //     cout << word << " ";
+    // }
+    cout<<"Hello World\n";
+    cout<<"enter int : ";
+    int a;
+    cin>>a;
+    cout<<"a = "<<a;
+    cout<<endl;
+    return 0;
 }
